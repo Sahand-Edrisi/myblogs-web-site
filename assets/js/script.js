fb7d44581e1f723ec18ebca7cf2691737e351c5b
sahand-edc <sahand.edc@gmail.com>
2023-07-13 12:51:14 +0330

    responsiv

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,26 @@ document.getElementById("change-theme").addEventListener("click", (e) => {
       "transform: translate(0px);";
   }
 });
+
+document
+  .getElementById("change-theme-menubar")
+  .addEventListener("click", (e) => {
+    if (document.body.classList.contains("light-theme")) {
+      document.body.classList.replace("light-theme", "dark-theme");
+      document.getElementById("moon").style = "z-index: -1;";
+    } else {
+      document.body.classList.replace("dark-theme", "light-theme");
+      document.getElementById("moon").style = "z-index: 2;";
+    }
+  });
+
+
+
+  
+function openNav() {
+  document.getElementById("menubar-menu-group").style = " display:block;";
+}
+
+function closeNav() {
+  document.getElementById("menubar-menu-group").style = " display:none;";
+}
